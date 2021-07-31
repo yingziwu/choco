20d3fd1488ee1e8f4c7083020c11d18854396732
bgme <i@bgme.me>
2021-07-31 14:01:23 +0800

    add cppcrypt 1.4.3.8 add dokany v1.5.0.3000

---
+++ cppcryptfs/tools/chocolateyinstall.ps1
@@ -0,0 +1,31 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'cppcryptfs'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$baseUrl = "https://github.com/bailey27/cppcryptfs/releases/download/"
+$version = "1.4.3.8"
+
+$cppcryptfsArgs = @{
+  packageName    = $packageName
+  fileFullPath   = "$toolsDir\cppcryptfs.exe"
+  url            = "$baseUrl/$version/cppcryptfs32.exe"
+  url64bit       = "$baseUrl/$version/cppcryptfs.exe"
+  checksum       = 'E06626B5049DBEC36965F449BEF9B7338B8E944BECAF8C4B21BD0B2E203E5CB4'
+  checksumType   = 'sha256' 
+  checksum64     = 'FD1D17D40A1FC1316BAF1DA9537DAAC5AC3C7EDB11705BC0851B080EEFF7A47E'
+  checksumType64 = 'sha256' 
+}
+
+$cppcryptfsctlArgs = @{
+  packageName    = $packageName
+  fileFullPath   = "$toolsDir\cppcryptfsctl.exe"
+  url            = "$baseUrl/$version/cppcryptfsctl32.exe"
+  url64bit       = "$baseUrl/$version/cppcryptfsctl.exe"
+  checksum       = '71F91C9E0C29030DE7792C47757C02EE9F4CB349433A68AD131DE3C2C4FF8C9B'
+  checksumType   = 'sha256' 
+  checksum64     = 'D96B423BE7509E66ED69620BCB3C966FE83617C3622D4E1A8D5A6A87AD07C544'
+  checksumType64 = 'sha256' 
+}
+
+Get-ChocolateyWebFile @cppcryptfsArgs
+Get-ChocolateyWebFile @cppcryptfsctlArgs
---
+++ dokany/tools/chocolateyinstall.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'dokany'
+$baseUrl = "https://github.com/dokan-dev/dokany/releases/download/"
+$version = "v1.5.0.3000"
+$url = "$baseUrl/$version/Dokan_x86.msi"
+$url64 = "$baseUrl/$version/Dokan_x64.msi"
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'msi'
+  url            = $url
+  url64bit       = $url64
+
+  softwareName   = 'Dokan Library*'
+  checksum       = '31F16DC3905F1683EB9DAC87210E83E679161BC2B2F4E05C9568DC4880F93448'
+  checksumType   = 'sha256'
+  checksum64     = '701B56281010ADF9FC401D5189DD223B1B300D4D123980A32A57F242A6A651D1'
+  checksumType64 = 'sha256'
+
+  silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes = @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
